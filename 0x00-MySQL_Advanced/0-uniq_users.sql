9ce8deb4570095f4ad10d94e35ee718bcf7653c0
[esekyi] <[sskert10@gmail.com]>
2024-10-18 05:37:54 +0000

    [fixes🛠️] added if not exists to sql command

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- SQL script that creates a table users
 -- with a unique constraint on the column email
-CREATE TABLE users (
-	id SERIAL PRIMARY KEY NOT NULL,
-	email VARCHAR(255) NOT NULL unique,
+CREATE TABLE IF NOT EXISTS users (
+	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255)
-) IF NOT EXISTS;
+);
