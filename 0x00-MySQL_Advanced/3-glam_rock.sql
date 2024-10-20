0da4935be0a22c34a8b735a5e269c158826e7010
[esekyi] <[sskert10@gmail.com]>
2024-10-20 05:38:01 +0000

    3. Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,2 +1,8 @@
 -- SQL script that lists all bands with Glam rock
 -- as their main style genre
+SELECT band_name,
+(CASE 
+WHEN split IS NULL THEN 2022 - formed
+ELSE split - formed
+END) AS lifespan FROM metal_bands
+WHERE style = 'Glam rock' ORDER BY lifespan DESC;
